620fd580a824cdb29297a3a553a7be5bf3c0cc54
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-03 06:00:45 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+async function getNavItems() {
+  const navItems = await fetch('https://api.example.com/...');
+  return navItems.json();
+}
+
+export default async function Layout({ children }) {
+  const navItems = await getNavItems();
+
+  return (
+    <>
+      <nav>
+        <ul>
+          {navItems.map((item) => (
+            <li key={item.id}>
+              <Link href={item.href}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
